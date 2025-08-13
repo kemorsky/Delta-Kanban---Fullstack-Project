09d99bb777448194922c09493712ad2fb5dd3f54
Kamil Romanowicz <143507032+kemorsky@users.noreply.github.com>
2025-08-13 10:28:12 +0200

    update controller

--- backend/src/controllers/authController.js
+++ backend/src/controllers/authController.js
@@ -50,8 +50,8 @@ export const login = async (req, res) => {
     );
 
     res.cookie('token', token, {
-      httpOnly: true,
-      secure: process.env.NODE_ENV, // send only over HTTPS in prod
+      httpOnly: false,
+      secure: process.env.NODE_ENV === 'production', // send only over HTTPS in prod
       sameSite: 'None',
       maxAge: 3600000, // 1 hour
     });
