a0965d62bc723b7bd7ed40a054903426f5c55259
Kamil Romanowicz <143507032+kemorsky@users.noreply.github.com>
2025-08-15 10:51:22 +0200

    fix mobile login error

--- backend/src/controllers/authController.js
+++ backend/src/controllers/authController.js
@@ -51,7 +51,7 @@ export const login = async (req, res) => {
 
     res.cookie('token', token, {
       httpOnly: true,
-      secure: process.env.NODE_ENV === 'production', // send only over HTTPS in prod
+      secure: true, // send only over HTTPS in prod
       sameSite: process.env.NODE_ENV === 'production' ? 'None' : 'Lax',
       maxAge: 3600000, // 1 hour
     });
