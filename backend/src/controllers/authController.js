544c80214ad529312433cb15523870f480af7f7a
Kamil Romanowicz <143507032+kemorsky@users.noreply.github.com>
2025-08-16 18:42:54 +0200

    Update authController.js

--- backend/src/controllers/authController.js
+++ backend/src/controllers/authController.js
@@ -52,7 +52,7 @@ export const login = async (req, res) => {
     res.cookie('token', token, {
       httpOnly: true,
       secure: true, // send only over HTTPS in prod
-      sameSite: 'None',
+      sameSite: 'none',
       maxAge: 3600000, // 1 hour
     });
 
