5f5075e98925a058349bce89fd591e8b119231b5
Kamil Romanowicz <143507032+kemorsky@users.noreply.github.com>
2025-08-16 11:13:59 +0200

    Update dbConnect.js

--- backend/src/config/dbConnect.js
+++ backend/src/config/dbConnect.js
@@ -1,11 +1,23 @@
 import mongoose from 'mongoose';
 
+let cached = global.mongoose;
+
+if (!cached) {
+  cached = global.mongoose = { conn: null, promise: null };
+}
+
 export const dbConnect = async () => {
-    try {
-        const connect = await mongoose.connect(process.env.CONNECTION_STRING);
-        console.log(`Database connected: ${connect.connection.host}, ${connect.connection.name}`);
-    } catch (err) {
-        console.log(err);
-        process.exit(1);
+    if (cached.conn) {
+        return cached.conn;
     }
+
+  if (!cached.promise) {
+    cached.promise = mongoose.connect(process.env.CONNECTION_STRING, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    }).then((mongoose) => mongoose);
+  }
+
+  cached.conn = await cached.promise;
+  return cached.conn;
 };
