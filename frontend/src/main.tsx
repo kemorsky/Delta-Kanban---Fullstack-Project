fffea8a03c5b9b30b5914eba2334cab7bc666f33
Kamil Romanowicz <143507032+kemorsky@users.noreply.github.com>
2025-09-02 12:27:32 +0200

    Update main.tsx

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -20,7 +20,7 @@ createRoot(document.getElementById('root')!).render(
 <QueryClientProvider client={queryClient}>
       <StrictMode>
         <RouterProvider router={router} />
-        <Analytics />
+        {/* <Analytics /> */}
       </StrictMode>
 </QueryClientProvider>
 )
